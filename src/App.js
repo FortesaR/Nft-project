0b1f6daf10b655e098fefe7a4302124656f5cdc3
Fortesa <fr47258@ubt-uni.net>
2024-07-08 16:21:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import ScrollToTop from "./components/ScrollToTop";
+import ScrollReveal from 'scrollreveal';
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Free from "./components/Free";
+import Clients from  "./components/Clients";
+import SuperRare from "./components/SuperRare";
+import Release from "./components/Release";
+import Like from "./components/Like";
+import Signup from "./components/Signup";
+import Footer from "./components/Footer";
+import "./scss/index.scss";
 
-function App() {
+export default function App() {
+  const [theme, setTheme] = useState("dark");
+  const appContainerRef = useRef(null);
+
+  const changeTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  useEffect(() => {
+    const registerAnimations = () => {
+      const sr = ScrollReveal({
+        origin: "bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false,
+      });
+      sr.reveal('.nav, .home, .free, .clients, .super-rare, .release, .like, .signup, .footer', {
+        interval: 500
+      });
+    };
+
+    registerAnimations();
+  }, []);
+
+  useEffect(() => {
+    // This useEffect handles the initial DOM manipulation
+    const home = appContainerRef.current.querySelector(".home");
+    const nav = appContainerRef.current.querySelector(".nav");
+
+    if (home && nav) {
+      setTimeout(() => {
+        home.style.transform = "none";
+        nav.style.transform = "none";
+      }, 1500);
+    }
+  }, [appContainerRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container' ref={appContainerRef} data-theme={theme}>
+      <ScrollToTop />
+      <Navbar changeTheme={changeTheme} currentTheme={theme} />
+      <Home />
+      <Free />
+      <Clients />
+      <SuperRare />
+      <Release />
+      <Like />
+      <Signup />
+      <Footer />
     </div>
   );
 }
-
-export default App;
